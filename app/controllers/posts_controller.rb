74ec6e9f68fdcfaf48f5e7e0126111991bb4cdb0
totzYuta <yuta.totz@gmail.com>
2015-09-26 20:46:00 +0900

    YATTERU! registering system

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,21 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    if Post.create(title: params[:post][:title])
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def post_params
+      params.require(:post).permit(:title)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root "posts#index"
+  resources :posts
+  # get "posts#new"
+  # post "posts#create"
   match "/auth/:provider/callback" => "sessions#create", :via => [:get, :post]
   match "/signout" => "sessions#destroy", :via => [:get, :delete]
 end
